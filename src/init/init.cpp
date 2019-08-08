f235cafa8f4dba4e4a55c666670e0fb1777fe1f1
przemykomo <przemyslawsowa15@gmail.com>
2019-08-08 11:15:26 +0200

    initOpenGL

--- src/init/init.cpp
+++ src/init/init.cpp
@@ -1,4 +1,5 @@
 #include "init.hpp"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "window/window.hpp"
 
@@ -22,6 +23,26 @@ void init::initGLFW(const int width, const int height, const char[] title) {
     //glfwSetKeyCallback(Window::GLFWwindowptr, keyCallback);
     //glfwSetWindowSizeCallback(Window::GlFWwindowptr, onWindowSizeChanged);
 
+    glfwSwapInterval(1);
+
     // make cursor invisible and lock it in center of window
     glfwSetInputMode(Window::GLFWwindowptr, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+}
+
+void init::initOpenGL() {
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        throw std::runtime_error("Failed to initialize OpenGL context! Possible solution: Check graphics drivers.");
+    }
+
+    int width, height;
+    glfwGetFramebufferSize(Window::GlFWwindowptr, &width, &height);
+    glViewport(0, 0, width, height);
+
+    glClearColor(0.07f, 0.3f, 0.8f, 1.0f);
+
+    glEnable(GL_DEPTH_TEST);
+}
+
+void init::initShaders() {
+    
 }
